588664eef17c33b9a60f13d351fb9485cdc7de1c
samuelmwangi729 <samuelmwangi729@gmail.com>
2024-06-04 08:16:48 +0300

    created a login request success

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -1,27 +1,56 @@
 import React, { useState } from 'react'
-import { View, Text, SafeAreaView, StatusBar, TextInput, TouchableOpacity, Pressable, KeyboardAvoidingView } from 'react-native'
+import { View, Text, SafeAreaView, StatusBar, TextInput, TouchableOpacity, Pressable, KeyboardAvoidingView, ActivityIndicator } from 'react-native'
 
 const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [errors, setErrors] = useState({})
+    const [login_error, setLoginError] = useState("")
+    const [login_success, setLoginSuccess] = useState("")
+    const [loading, setLoading] = useState(false)
     const validateForm = () => {
         //validate the fields
         if (!username) {
-            setErrors({"username":"Please Enter a username"})
+            setErrors({ "username": "Please Enter a username" })
         } else if (!password) {
-            setErrors({"password":"Please Enter a password"})
+            setErrors({ "password": "Please Enter a password" })
         }
         else {
             return true
         }
     }
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (validateForm()) {
-            console.log(`Submit ${username} and ${password}`)
+            //submit the data to the backend server 
+            setLoading(true)
+            try {
+                const response = await fetch("http://192.168.100.10:8080/api/login", {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        email: username,
+                        password: password
+                    })
+                })
+                const login_response = await response.json()
+                if (login_response.error) {
+                    setLoading(false)
+                    setLoginSuccess("")
+                    setLoginError(login_response.error.message)
+                } else {
+                    setLoading(false)
+                    setLoginError("")
+                    setLoginSuccess("Login successful")
+                    console.log(login_response)
+                }
+            } catch (error) {
+                console.log(error)
+            }
         }
     }
     const handleUsernameChange = (text) => {
+        setLoginError("")
+        setLoading(false)
         if ((text).toString().length === 0) {
             setErrors({ "username": "Username cannot be empty" })
             setUsername('')
@@ -52,6 +81,14 @@ const Login = () => {
                 <Text className="text-center font-bold text-xl px-2 py-5">
                     Login Your Account
                 </Text>
+                {
+                    login_error ? (
+                        <Text className="text-center text-red-700">{login_error}</Text>
+                    ) : null
+                }
+                {
+                    login_success ? (<Text className="text-center text-green-600">{login_success}</Text>) : null
+                }
                 <View>
                     <Text className="font-bold text-md">
                         Username
@@ -67,13 +104,24 @@ const Login = () => {
                         errors.password ? <Text className="text-red-600">{errors.password}</Text> : null
                     }
                     <TextInput defaultValue={password} className={`h-10 px-2 ${errors.password ? "border-b border-red-500 m-1" : "border-b"}`} placeholder="Enter your Password Here" secureTextEntry={true} autoCapitalize={false} autoCorrect={false} onChangeText={newPassword => handlePasswordChange(newPassword)} />
-                    <Pressable>
-                        <TouchableOpacity className="w-full bg-[#ff6600] p-4 mt-5 rounded-lg" onPress={handleSubmit}>
-                            <Text className="text-center text-white font-bold text-xl">
-                                Login
+
+                    {loading ? (
+                        <View className="bg-[#ff6600] mt-3">
+                            <ActivityIndicator size={'large'} color={"white"} />
+                            <Text className="text-center text-white mb-2">
+                                Please Wait
                             </Text>
-                        </TouchableOpacity>
-                    </Pressable>
+                        </View>
+                    ) : (
+                        <Pressable >
+                            <TouchableOpacity className="w-full bg-[#ff6600] p-4 mt-5 rounded-lg" onPress={handleSubmit}>
+                                <Text className="text-center text-white font-bold text-xl">
+                                    Login
+                                </Text>
+                            </TouchableOpacity>
+                        </Pressable>
+                    )}
+
                 </View>
                 <Text className="text-center mt-5 font-bold text-lg">
                     Or Login With ...
